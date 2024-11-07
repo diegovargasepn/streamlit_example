e02e5174574573313e1427f9b96ed2170eedba03
unknown <you@example.com>
2024-11-07 15:03:43 -0500

    Modification of the gif

--- streamlit_example.py
+++ streamlit_example.py
@@ -12,3 +12,9 @@ st.subheader("Como estás?")
 st.write("Adios amor")
 
 st.dataframe(df)
+
+gif_url = 'https://tenor.com/es/view/porquinho-polidance-gif-26422948'
+
+st.image(gif_url)
+
+st.image(gif_url, use_column_width=True)
